1963e0fe5d0316948cad6bc93bcee92f90a09551
{kon0615} <{yanari06152@yahoo.co.jp}>
2024-03-09 16:05:02 +0900

    mac

--- scriptCode.py
+++ scriptCode.py
@@ -1,18 +1,19 @@
 from selenium import webdriver
-
+from selenium.webdriver.remote  import webelement
 from selenium.webdriver.common import by    
 from selenium.webdriver.chrome.options import Options
-import chromedriver_binary
-import time
+#macOSの場合はchromedriver_binaryはコメントアウト
+#import chromedriver_binary
+import time 
 
 class Scrayp:
     targetURL =r"https://ana-slo.com/%e3%83%9b%e3%83%bc%e3%83%ab%e3%83%87%e3%83%bc%e3%82%bf/"
-    
+    ad = '#google_vignette'
     def __init__(self,Area:str) -> None:
         self.areaName = Area
-        self.star_webdriver()
+        self.start_webdriver()
         
-    def star_webdriver(self):
+    def start_webdriver(self):
         options = Options()
         options.add_argument('--disable-popup-blocking')
         self.driver = webdriver.Chrome(options=options)
@@ -23,21 +24,35 @@ class Scrayp:
         #js_code = code
         
         #ADが出るようなところは更新をしてAD回避
+
         self.driver.refresh() 
-        time.sleep(5)
-        Touhoku2 = self.driver.find_element(by.By.LINK_TEXT,self.areaName)
-        Touhoku2.click()
+        
+        Touhoku2 = self.driver.find_elements(by.By.CLASS_NAME,'simple_square_btn')
+        
+        while(self.driver.current_url ==self.ad):
+            self.driver.refresh()
+            self.SearchForAreaButton(Touhoku2)
+        
+        
     def Start_scrayping(self,startday:str,Endday:str):
         ##とりあえず全部
-        Touhoku = self.driver.find_element(by.By.LINK_TEXT,self.areaName)
+        MainMenu = self.driver.find_elements(by.By.CLASS_NAME,'simple_square_btn')
+        self.driver.get(self.targetURL + self.areaName)
+        
         
         #######
-        Touhoku.click()
+        time.sleep(5)
+
         #広告が出るときがある
-        ad = '#google_vignette'
-        if(ad in self.driver.current_url):
-            ##広告が出たら更新
-            self.AD_blocker()
+        
+
+    def SearchForAreaButton(self,listBt:list[webelement.WebElement]):
+        #メインメニューから指定エリアのボタンを探してクリック
+        for item in  listBt:
+            if item.text == self.areaName:
+                print('catch')
+                
+                item.click()
+                return
         
         
-        print("")
